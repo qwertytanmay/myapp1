43eb9a2432a029bac303ea46c863d90e60f5b91b
qwertytanmay <99901740+qwertytanmay@users.noreply.github.com>
2022-10-02 15:57:37 +0530

    Add files via upload

---
+++ myApp/myFiles/contest/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react"
+const App = () => {
+    const [input, setInput] = useState("");
+    const [display, setDisplay] = useState("");
+
+ const whileTyping = (event) => {
+    event.preventDefault();
+    setInput(event.target.value)
+ }
+ const final = () => {
+    setDisplay(input)
+ }
+return(
+    <>
+        <input id="input" onChange={whileTyping}></input>
+        <p id="text">{display}</p>
+        <button id="button" onClick={final}></button>
+    </>
+)
+}
+
+export default App;
